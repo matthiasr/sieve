047462e4c9ab29e3d2108f823a1349942fcd8c2c
Matthias Rampke <matthias@rampke.de>
2014-09-08 21:20:47 +0200

    Irken

---
+++ irken/README.md
@@ -0,0 +1,4 @@
+Irken
+====
+
+[Irken](https://github.com/samrushing/irken-compiler) is a typed scheme.
---
+++ irken/primes.scm
@@ -0,0 +1,16 @@
+(include "lib/core.scm")
+(include "lib/pair.scm")
+
+(define isprime?
+  n () -> #t
+  n (p . ps) -> (if (zero? (remainder n p))
+                #f
+                (isprime? n ps)))
+
+(define (primes limit n ps)
+  (cond
+    ((>= n limit) ps)
+    ((isprime? n ps) (primes limit (+ n 1) (cons n ps)))
+    (else (primes limit (+ n 1) ps))))
+
+(primes 100 2 (list:nil))
