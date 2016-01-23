f4d67188c7a8028a0cd4c22233b658ae47aa048d
Matthias Rampke <matthias@rampke.de>
2016-01-23 20:37:38 +0100

    Rename to make compilable
    make internal methods private
    
    remove direct execution

---
+++ elixir/simple.ex
@@ -0,0 +1,17 @@
+defmodule SimpleSieve do
+  defp isprime(n, primes) when is_list(primes) and is_integer(n) do
+    !Enum.any?(primes, &(rem(n,&1) == 0))
+  end
+
+  defp primes(maxn, n, ps) when is_integer(n) and is_integer(maxn) and n <= maxn and n > 0 do
+    cond do
+      n == maxn -> ps
+      isprime n, ps -> primes maxn, n+1, [n|ps]
+      true -> primes maxn, n+1, ps
+    end
+  end
+
+  def primes(maxn) when maxn > 0 do
+    primes(maxn,3,[2])
+  end
+end
