f8b00f086087b1c2588f115e3210ead91f8b6b74
Matthias Rampke <matthias@rampke.de>
2016-01-23 17:13:43 +0100

    very simple Elixir

---
+++ elixir/simple.exs
@@ -0,0 +1,23 @@
+defmodule Sieve do
+  def isprime(n, primes) when is_list(primes) and is_integer(n) do
+    case primes do
+      [] -> true
+      [p|ps] -> if rem(n,p) == 0, do: false, else: isprime(n,ps)
+    end
+  end
+
+  def primes(maxn, n, ps) when is_integer(n) and is_integer(maxn) and n <= maxn and n > 0 do
+    cond do
+      n == maxn -> ps
+      isprime n, ps -> primes maxn, n+1, [n|ps]
+      true -> primes maxn, n+1, ps
+    end
+  end
+
+  def primes(maxn) when maxn > 0 do
+    primes(maxn,3,[2])
+  end
+end
+
+#Enum.each(Sieve.primes(100000), &IO.puts/1)
+IO.puts hd Sieve.primes(100000)
