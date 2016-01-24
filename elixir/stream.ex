c162997c42ccae9f0635c6d531f118bd45a2db1a
Matthias Rampke <matthias@rampke.de>
2016-01-24 10:58:05 +0100

    Fix StreamSieve
    by the power of unfold

--- elixir/stream.ex
+++ elixir/stream.ex
@@ -1,20 +1,20 @@
 defmodule StreamSieve do
-  def ismult(n, ps) when is_integer(n) do
+  def isprime(n, ps) when is_integer(n) do
     !Enum.all?(
       Stream.take_while(ps, &(&1 < n)),
       &(rem(n,&1) == 0)
     )
   end
 
-  def primes(n, ps) do
+  def prime_generator [ n: n, ps: ps ] do
     cond do
-      ismult(n, ps) -> primes( n+1, Stream.concat(ps,[n]))
-      true -> primes(n+1, ps)
+      isprime(n, ps) -> { n, [ n: (n+1), ps: ps ] }
+      true -> prime_generator [ n: n+1, ps: ps ]
     end
   end
 
   def primes do
-    primes(2,[])
+    Stream.unfold([ n: 3, ps: [2] ], &prime_generator/1)
   end
 
   def primes n do
